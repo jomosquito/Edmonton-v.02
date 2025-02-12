83551aec5b47dd000a4bf5ad537670bccd8a694a
J <62847095+johnpaul123Z@users.noreply.github.com>
2025-02-11 19:07:15 -0600

    added more html and css files and started  sql

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+class profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
 
+    def __repr__(self):
+        return '<Task %r>' % self.id
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') #acceses index.html file gi
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  
     app.run(debug=True)
